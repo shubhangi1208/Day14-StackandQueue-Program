f60673cb196e29a9a346a100466dc2773d54b00c
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-21 17:58:53 +0530

    ADDED:User can add data in Queue

--- src/com/bridgelabz/StackQueue.java
+++ src/com/bridgelabz/StackQueue.java
@@ -11,23 +11,30 @@ public class StackQueue extends LinkedList {
         int choice;
         choice = sc.nextInt();
         do {
-            System.out.println("\n 1.Push in Stack \n 2.Pop from Stack \n 3.Display Stack \n 4.Quit ");
+            System.out.println("\n 1.Push in Stack \n 2.Pop from Stack \n 3.Display Stack \n 4.Insert data in in Queue \n 4.Quit ");
             switch (choice) {
                 case 1:
                     System.out.println("Enter the Number");
                     int number = sc.nextInt();
                     stackQueue.insertFirst(number);
+                    break;
                 case 2:
                     stackQueue.pop();
                     break;
                 case 3:
                     stackQueue.showList();
+                    break;
                 case 4:
+                    System.out.println("Enter the data which you want to add in Queue");
+                    int data = sc.nextInt();
+                    stackQueue.insertLast(data);
+                    break;
+                case 5:
                     System.out.println("Thank you");
                     break;
             }
 
-        } while (choice != 4);
+        } while (choice != 5);
     }
 
 }
